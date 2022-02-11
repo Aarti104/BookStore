3938823e47ce71aef08ef43511f3cb1d5338cf9d
Aarti104 <93678063+Aarti104@users.noreply.github.com>
2022-02-11 16:15:04 +0530

    Add files via upload

--- BookStoreManagementApplication/BookStoreManagementApplication/src/main/java/com/cg/bookstore/entity/Author.java
+++ BookStoreManagementApplication/BookStoreManagementApplication/src/main/java/com/cg/bookstore/entity/Author.java
@@ -11,105 +11,61 @@ import javax.persistence.SequenceGenerator;
 public class Author {
 	
 	@Id
-	@SequenceGenerator(name="mylogic",initialValue=100,allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.AUTO,generator="mylogic")
-	private int id;
-	
-	private String name;
-	
-	private String email;
-	
-	private int no_of_books; //unique count of books published by author
-	
-
-	public int getId() {
-		return id;
-	}
-
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-
-	public String getName() {
-		return name;
-	}
-
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-	public int getNo_of_books() {
-		return no_of_books;
-	}
-
-
-
-	public void setNo_of_books(int no_of_books) {
-		this.no_of_books = no_of_books;
-	}
-
-
-
+	@SequenceGenerator(name="myauthorlogic",initialValue=100,allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.AUTO,generator="myauthorlogic")
+	private int authorid;
+	private String authorname;
+	private String autoremail;
+	private int no_of_books;
 	public Author() {
-		
-	}
-
-	
-
-	public Author(String name, String email, int no_of_books) {
 		super();
-		this.name = name;
-		this.email = email;
+		// TODO Auto-generated constructor stub
+	}
+	public Author(int authorid, String authorname, String autoremail, int no_of_books) {
+		super();
+		this.authorid = authorid;
+		this.authorname = authorname;
+		this.autoremail = autoremail;
 		this.no_of_books = no_of_books;
 	}
-
-
-
-	public Author(int id, String name, String email, int no_of_books) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.no_of_books = no_of_books;
-	}
-	
 	@Override
 	public int hashCode() {
-		return Objects.hash(email, id, name, no_of_books);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + authorid;
+		result = prime * result + ((authorname == null) ? 0 : authorname.hashCode());
+		result = prime * result + ((autoremail == null) ? 0 : autoremail.hashCode());
+		result = prime * result + no_of_books;
+		return result;
 	}
-
-
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Author))
 			return false;
 		Author other = (Author) obj;
-		return Objects.equals(email, other.email) && id == other.id && Objects.equals(name, other.name)
-				&& no_of_books == other.no_of_books;
+		if (authorid != other.authorid)
+			return false;
+		if (authorname == null) {
+			if (other.authorname != null)
+				return false;
+		} else if (!authorname.equals(other.authorname))
+			return false;
+		if (autoremail == null) {
+			if (other.autoremail != null)
+				return false;
+		} else if (!autoremail.equals(other.autoremail))
+			return false;
+		if (no_of_books != other.no_of_books)
+			return false;
+		return true;
 	}
+	@Override
+	public String toString() {
+		return "Author [authorid=" + authorid + ", authorname=" + authorname + ", autoremail=" + autoremail
+				+ ", no_of_books=" + no_of_books + "]";
+	} 
+
+	
 }
