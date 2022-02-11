418fdf7a45dcaa0cc0a699284195296735c4452f
Aarti104 <93678063+Aarti104@users.noreply.github.com>
2022-02-11 15:08:48 +0530

    Add files via upload

--- BookStoreManagementApplication/BookStoreManagementApplication/src/main/java/com/cg/bookstore/entity/Order.java
+++ BookStoreManagementApplication/BookStoreManagementApplication/src/main/java/com/cg/bookstore/entity/Order.java
@@ -2,39 +2,38 @@ package com.cg.bookstore.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Order {
 	
-	private String orderId;
-	private String customerId;
+	@Id
+	@SequenceGenerator(name="myorderlogic",initialValue=700,allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.AUTO,generator="myorderlogic")
+	private int orderId;
 	private String orderStatus;
 	private LocalDate orderDate;
 	private double totalPrice;
-	public Order() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Order(String orderId, String customerId, String orderStatus, LocalDate orderDate, double totalPrice) {
-		super();
-		this.orderId = orderId;
-		this.customerId = customerId;
-		this.orderStatus = orderStatus;
-		this.orderDate = orderDate;
-		this.totalPrice = totalPrice;
-	}
-	public String getOrderId() {
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinTable(
+			name="Customer_order_Info",
+			joinColumns=@JoinColumn(name="orderdetails"),
+			inverseJoinColumns=@JoinColumn(name="customerdetails"))
+
+	public int getOrderId() {
 		return orderId;
 	}
-	public void setOrderId(String orderId) {
+	public void setOrderId(int orderId) {
 		this.orderId = orderId;
-	}
-	public String getCustomerId() {
-		return customerId;
-	}
-	public void setCustomerId(String customerId) {
-		this.customerId = customerId;
 	}
 	public String getOrderStatus() {
 		return orderStatus;
@@ -56,16 +55,15 @@ public class Order {
 	}
 	@Override
 	public String toString() {
-		return "Order [orderId=" + orderId + ", customerId=" + customerId + ", orderStatus=" + orderStatus
-				+ ", orderDate=" + orderDate + ", totalPrice=" + totalPrice + "]";
+		return "Order [orderId=" + orderId + ", orderStatus=" + orderStatus + ", orderDate=" + orderDate
+				+ ", totalPrice=" + totalPrice + "]";
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
 		result = prime * result + ((orderDate == null) ? 0 : orderDate.hashCode());
-		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		result = prime * result + orderId;
 		result = prime * result + ((orderStatus == null) ? 0 : orderStatus.hashCode());
 		long temp;
 		temp = Double.doubleToLongBits(totalPrice);
@@ -79,20 +77,12 @@ public class Order {
 		if (!(obj instanceof Order))
 			return false;
 		Order other = (Order) obj;
-		if (customerId == null) {
-			if (other.customerId != null)
-				return false;
-		} else if (!customerId.equals(other.customerId))
-			return false;
 		if (orderDate == null) {
 			if (other.orderDate != null)
 				return false;
 		} else if (!orderDate.equals(other.orderDate))
 			return false;
-		if (orderId == null) {
-			if (other.orderId != null)
-				return false;
-		} else if (!orderId.equals(other.orderId))
+		if (orderId != other.orderId)
 			return false;
 		if (orderStatus == null) {
 			if (other.orderStatus != null)
