fbbf728b8a1134df42665d4af950e6a29ad37fac
Aarti104 <93678063+Aarti104@users.noreply.github.com>
2022-02-11 13:00:54 +0530

    Add files via upload

---
+++ BookStoreManagementApplication/BookStoreManagementApplication/src/main/java/com/cg/bookstore/entity/Book.java
@@ -0,0 +1,69 @@
+package com.cg.entity;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+@NamedQueries(@NamedQuery(name = "getAllBooks", query = "SELECT book FROM Book book"))
+public class Book implements Serializable {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="book_id")
+	private Integer id;
+	@Column(name="title")
+	private String title;
+	@Column(name="author")
+	private String author;
+	@Column(name="price")
+	private Double price;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author
+				+ ", price=" + price + "]";
+	}
+
+}
